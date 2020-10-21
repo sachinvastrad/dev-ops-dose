2a87388f7285ff1993302854856a5c8280a75a1c
sachinv18 <sachinv1821@gmail.com>
2020-10-21 21:13:25 +0530

    Day12 and Day 13
    Day12 and Day 13

---
+++ Day13/resource/main.tf
@@ -0,0 +1,19 @@
+# Configure the Azure provider
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "PenclinicRG"
+  location = "westus2"
+}
